038911738ea70a5b68f5c1b6bbd873ba362bcf99
kunomaclis <mitchellhendee@gmail.com>
2017-05-09 18:37:44 -0500

    add print statment for hamster program

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -8,13 +8,22 @@
     puts "Adoptable? y/n?"
     adoptable = gets.chomp
       if adoptable == "y"
-          adoptable == true
+          adoption_chance = true
       elsif adoptable == "n"
-          adoptable == false
+          adoption_chance = false
         else 
-            adoptable == nil
+        adoption_chance = nil
     end 
     puts "how old is hamster?"
     age = Integer(gets.chomp)
 
+    print "Awesome, your hamster's name is #{hamster_name}. It's volume from 1-10 is #{volume_level}. It's fur color is #{fur_color}. It is also #{age} years old."
 
+    if adoption_chance == true
+        puts " This hamster is ready for adoption!"
+    elsif adoption_chance == false 
+        puts " This hamster is covered in disease and not ready for adoption"
+    elsif adoption_chance == nil
+        puts " Is this even a hamster?"
+    end 
+    
