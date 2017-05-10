28b5c08cf2b47263d9f83afb6affa2cb253fdb2f
kunomaclis <mitchellhendee@gmail.com>
2017-05-10 16:50:45 -0500

    Sorting algorithm for release 3

--- ruby/temp-vtcclqczli.rb
+++ ruby/temp-vtcclqczli.rb
@@ -1,5 +1,4 @@
-# Vampire Detector Quiz + condition
-# The Quiz
+# Vampire Detector Quiz
 
 puts "Welcome to Werewolf Inc. What's your name?"
 name = gets.chomp
@@ -8,8 +7,8 @@ vampire = true if name == 'Drake Cula' || name == 'Tu Fang'
 
 puts 'How old are you? What year were you born?'
 age = Integer(gets.chomp)
-age_okay = false
 age_okay = true if age < 100
+age_suspect = true if age > 100
 
 puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
 garlic_preference = gets.chomp
@@ -18,22 +17,18 @@ garlic_yes = true if garlic_preference == 'yes' || garlic_preference == 'y'
 
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
-yes_insurance = false
-yes_insurance = true if insurance == 'yes' || insurance == 'y'
+if insurance == 'yes' || insurance == 'y'
+  yes_insurance = true
+elsif insurance == 'no' || insurance == 'n'
+  yes_insurance = false
+end
 
-# The results and sorting algorithms (How can I make the lines shorter?!?!)
-
-if vampire == false  && age_okay && garlic_yes && yes_insurance
+if age_okay == true && garlic_yes == true || yes_insurance == true
   puts 'Probably not a vampire'
-end
-
-if vampire == false && age_okay != true || garlic_yes != true || yes_insurance != true
+elsif age_suspect == true || garlic_yes == false || yes_insurance == false
   puts 'Probably a vampire'
-end
-
-if vampire == false && (age_okay == false && garlic_yes == false && yes_insurance == false)
+elsif age_suspect == true && garlic_yes == false && yes_insurance == false
   puts 'Almost certainly a vampire'
+elsif vampire == true
+  puts 'Definitely a vampire'
 end
-# end condition
-
-puts 'Definitely a vampire' if vampire == true
