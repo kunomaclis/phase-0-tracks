ed38e318436d847a81d743e9519f76756fe404fa
kunomaclis <mitchellhendee@gmail.com>
2017-05-09 21:41:01 -0500

    Add framework for sorting

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,7 +2,10 @@
 
 puts "Welcome to Werewolf Inc. What's your name?"
 name = gets.chomp
-vampire = true if name == 'Drake Cula' || 'Tu Fang'
+vampire = false
+if name = 'Drake Cula' || 'Tu Fang'
+  vampire = true
+end
 
 puts 'How old are you? What year were you born?'
 age = Integer(gets.chomp)
@@ -11,14 +14,19 @@ age_suspect = true if age > 100
 
 puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
 garlic_preference = gets.chomp
-garlic_yes = true if garlic_preference == "yes"
-else garlic_yes = false 
+garlic_yes = false
+garlic_yes = true if garlic_preference == 'yes'
 
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
-if insurance == "yes" 
-    yes_insurance = true
-elsif insurance == "no"
-    yes_insurance = false
-else yes_insurance = nil 
-end 
+if insurance == 'yes'
+  yes_insurance = true
+elsif insurance == 'no'
+  yes_insurance = false
+end
+
+puts 'Probably not a vampire' if age_okay && garlic_yes == true || yes_insurance == true
+puts 'Probably a vampire' if age_suspect == true || garlic_yes == false || yes_insurance == false
+puts 'Almost certainly a vampire' if age_suspect == true && garlic_yes == false && yes_insurance == false
+puts 'Definitely a vampire' if vampire == true
+
