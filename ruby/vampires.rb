e68dd4ae4915191d2c73116f8bcab0347cc549ec
kunomaclis <mitchellhendee@gmail.com>
2017-05-09 21:54:28 -0500

    add further formatting

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,9 +3,7 @@
 puts "Welcome to Werewolf Inc. What's your name?"
 name = gets.chomp
 vampire = false
-if name = 'Drake Cula' || 'Tu Fang'
-  vampire = true
-end
+vampire = true if name == 'Drake Cula' || name == 'Tu Fang'
 
 puts 'How old are you? What year were you born?'
 age = Integer(gets.chomp)
@@ -15,18 +13,22 @@ age_suspect = true if age > 100
 puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
 garlic_preference = gets.chomp
 garlic_yes = false
-garlic_yes = true if garlic_preference == 'yes'
+garlic_yes = true if garlic_preference == 'yes' || garlic_preference == 'y'
 
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
-if insurance == 'yes'
+if insurance == 'yes' || insurance == 'y'
   yes_insurance = true
-elsif insurance == 'no'
+elsif insurance == 'no' || insurance == 'n'
   yes_insurance = false
 end
 
-puts 'Probably not a vampire' if age_okay && garlic_yes == true || yes_insurance == true
-puts 'Probably a vampire' if age_suspect == true || garlic_yes == false || yes_insurance == false
-puts 'Almost certainly a vampire' if age_suspect == true && garlic_yes == false && yes_insurance == false
-puts 'Definitely a vampire' if vampire == true
-
+if age_okay == true && garlic_yes == true || yes_insurance == true
+  puts 'Probably not a vampire'
+elsif age_suspect == true || garlic_yes == false || yes_insurance == false
+  puts 'Probably a vampire'
+elsif age_suspect == true && garlic_yes == false && yes_insurance == false
+  puts 'Almost certainly a vampire'
+elsif vampire == true
+  puts 'Definitely a vampire'
+end
