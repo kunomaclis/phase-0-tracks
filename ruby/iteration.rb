7d7c280bd585edca0b8cf23a3504b36c8a25dcc3
kunomaclis <mitchellhendee@gmail.com>
2017-05-17 17:14:16 -0500

    Add hash and array function

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -8,3 +8,24 @@ def sup_bro
 end
 
 sup_bro { |name| puts "Here comes that block with a uniquie person called #{name}" }
+
+# Begin Array and Hash Block example .each and .map
+
+movie_stars = {
+  'Jim Carey' => 'Eternal Sunshine of the Spotless Mind',
+  'Brad Pitt' => 'Troy',
+  'Jennifer Aniston' => 'Meet the Fawkers'
+}
+
+movie_stars.each { |name, movie| puts "The actor #{name} peformed in the movie #{movie}" }
+
+dinos = ['T-Rex', 'Stegosaurus', 'Allosaurus']
+
+puts 'First, there were dinos:'
+puts dinos
+
+puts 'Then came the meteor!'
+dinos.map! do |target|
+  'Extinct.'
+end
+
