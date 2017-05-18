c2174d5f7f7405639321dd030ea9bd6e64c5324a
kunomaclis <mitchellhendee@gmail.com>
2017-05-17 21:27:43 -0500

    bug is the best cat 2k17

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -25,13 +25,48 @@ end
 puts 'Finally, what theme do you want your decor to be?'
 prefs[:decor_theme] = gets.chomp
 
-puts "Alright, here's all of the data I have so far..."
+puts "Alright, here's all of the data I have so far:"
 puts prefs
 
-puts 'Is there anything you missed or need changed?'
-change = gets.chomp
-if change == 'no' || change == 'n'
-  puts "awesome! Then I'll move forward with what you've decided"
-elsif change == 'yes' || change == 'y'
-  puts 'okay, what are we changing?'
+# Loop if questions missed protocol
+updated_info = false
+
+until updated_info
+
+  puts "Would you like to make any changes? If not, type 'none' otherwise type: name, age, kids, or decor theme"
+  new_info = gets.chomp
+  if new_info == 'name'
+    puts 'what is the new name?'
+    prefs[:name] = gets.chomp
+    updated_info = true
+  elsif new_info == 'age'
+    puts 'what is the new age?'
+    prefs[:age] = gets.chomp
+    updated_info = true
+  elsif new_info == 'kids'
+    puts 'Do you have any children?'
+    new_kids = gets.chomp
+    if new_kids == 'yes' || new_kids == 'y'
+      prefs[:kids] = true
+    elsif new_kids == 'no' || new_kids == 'n'
+    end
+    updated_info = true
+  elsif new_info == 'decor theme'
+    puts 'What do you want the decor to be like?'
+    prefs[:decor_theme] = gets.chomp
+    updated_info = true
+
+  elsif new_info == 'none'
+    puts 'Roger!'
+    updated_info = true
+  else
+    puts 'what?'
+    updated_info = false
+  end
 end
+
+puts 'Okay, then the updated client info is:'
+
+puts prefs
+
+puts 'Thanks for working with Hendee Design sucka!!!'
