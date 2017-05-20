e96d33ad298bf56a32498597cb33b2aba2ef4665
kunomaclis <mitchellhendee@gmail.com>
2017-05-20 14:20:48 -0500

    Answer a few hash drills

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -8,10 +8,13 @@ zombie_apocalypse_supplies = ['hatchet', 'rations', 'water jug', 'binoculars',
 
 # zombie_apocalypse_supplies.each { |x| print " *#{x}" }
 
+# --------------------------------------------------------------------------
+
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 
 # puts zombie_apocalypse_supplies.sort
+# --------------------------------------------------------------------------
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
@@ -28,6 +31,8 @@ zombie_apocalypse_supplies = ['hatchet', 'rations', 'water jug', 'binoculars',
 # end
 # item_checker
 
+# --------------------------------------------------------------------------
+
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
@@ -35,17 +40,22 @@ zombie_apocalypse_supplies = ['hatchet', 'rations', 'water jug', 'binoculars',
 # zombie_apocalypse_supplies.slice!(0..2)
 # print zombie_apocalypse_supplies
 
+# --------------------------------------------------------------------------
+
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
 # documentation for Arrays.
+
 other_survivor_supplies = ['warm clothes', 'rations', 'compass', 'camp stove',
                            'solar battery', 'flashlight']
-# ----
-updated_supplies = other_survivor_supplies + zombie_apocalypse_supplies
 
-puts updated_supplies.uniq.sort
+# updated_supplies = other_survivor_supplies + zombie_apocalypse_supplies
+
+# puts updated_supplies.uniq.sort
+
+# --------------------------------------------------------------------------
 
 # Hash Drills
 
@@ -61,17 +71,25 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+
+# extinct_animals.each { |ani, year| print "#{ani} -- #{year} * " }
+
+# --------------------------------------------------------------------------
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
-# ----
+
+p extinct_animals.select { |_key, value| value <= 2000 }
+
+# --------------------------------------------------------------------------
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+
+# --------------------------------------------------------------------------
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -81,8 +99,11 @@ extinct_animals = {
 # Do not use any special built-in methods.
 # ----
 
+# --------------------------------------------------------------------------
+
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+# --------------------------------------------------------------------------
