dd8b1e6d16c43ee0cce50d29861e1b358129d60c
kunomaclis <mitchellhendee@gmail.com>
2017-05-20 16:33:56 -0500

    Solve fibonacci generator

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -2,12 +2,22 @@
 # Let the user know what the index of the number is
 # print "Nil" if integer isn't present
 
-def search_array(array, n)
-  p array.rindex(n) if array.include?(n)
+# def search_array(array, n)
+#   p array.rindex(n) if array.include?(n)
+# end
+
+# arr = [2, 4, 6, 8]
+
+# search_array(arr, 6)
+
+# ------------------------------------------------
+
+def fibonacci(n)
+  fib_array = [0, 1]
+  repeat =	n - 2
+  repeat.times { fib_array << fib_array[-1] + fib_array[-2] }
+  p fib_array
 end
 
-arr = [2, 4, 6, 8]
-
-search_array(arr, 6)
-
+fibonacci(6)
 
