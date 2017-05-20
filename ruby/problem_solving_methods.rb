d0b0e2964fc928f7478b3848263f8931dd19e5c7
kunomaclis <mitchellhendee@gmail.com>
2017-05-20 17:15:31 -0500

    finish release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,12 +12,37 @@
 
 # ------------------------------------------------
 
-def fibonacci(n)
-  fib_array = [0, 1]
-  repeat =	n - 2
-  repeat.times { fib_array << fib_array[-1] + fib_array[-2] }
-  p fib_array
-end
+# def fibonacci(n)
+#   fib_array = [0, 1]
+#   repeat =	n - 2
+#   repeat.times { fib_array << fib_array[-1] + fib_array[-2] }
+#   p fib_array
+# end
 
-fibonacci(6)
+# fibonacci(6)
 
+#--------------------------------------------------
+
+# Bubble Sorting
+# Take in an array
+# Compare current array value with next index value. If value is greater than, swap places with next index value
+# Continue iterating through index values until there are no more comparisons to be made
+
+# def bubblez(array)
+#   n = array.length
+#   loop do
+#     swapped = false
+#     (n - 1).times do |i|
+#       next unless array[i] > array[i + 1]
+#       array[i], array[i + 1] = array[i + 1], array[i]
+#       swapped = true
+#       p array
+#     end
+#     break unless swapped
+#   end
+#   array
+# end
+
+# array = [1, 20, 50, 1, 3, 100, 50, 1]
+
+# bubblez(array)
