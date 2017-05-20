61f8b4ac7a6fe78e93349d843f31068cb1047749
kunomaclis <mitchellhendee@gmail.com>
2017-05-20 14:07:37 -0500

    Finish Array quiz

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,45 +1,62 @@
 # Array Drills
 
-zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-                              "shotgun", "compass", "CB radio", "batteries"]
+zombie_apocalypse_supplies = ['hatchet', 'rations', 'water jug', 'binoculars',
+                              'shotgun', 'compass', 'CB radio', 'batteries']
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
-# ----
+
+# zombie_apocalypse_supplies.each { |x| print " *#{x}" }
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
-# ----
+
+# puts zombie_apocalypse_supplies.sort
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
-# ----
+
+# def item_checker
+#   zombie_apocalypse_supplies = ['hatchet', 'rations', 'water jug', 'binoculars',
+#                                 'shotgun', 'compass', 'CB radio', 'batteries']
+#   if zombie_apocalypse_supplies.include?('boots')
+#     puts 'We have boots!'
+#   else
+#     puts 'I SEE NO BOOTS!!!'
+#   end
+# end
+# item_checker
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
-# ----
+
+# zombie_apocalypse_supplies.slice!(0..2)
+# print zombie_apocalypse_supplies
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
 # documentation for Arrays.
-other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
-                            "solar battery", "flashlight"]
+other_survivor_supplies = ['warm clothes', 'rations', 'compass', 'camp stove',
+                           'solar battery', 'flashlight']
 # ----
+updated_supplies = other_survivor_supplies + zombie_apocalypse_supplies
+
+puts updated_supplies.uniq.sort
 
 # Hash Drills
 
 extinct_animals = {
-  "Tasmanian Tiger" => 1936,
-  "Eastern Hare Wallaby" => 1890,
-  "Dodo" => 1662,
-  "Pyrenean Ibex" => 2000,
-  "Passenger Pigeon" => 1914,
-  "West African Black Rhinoceros" => 2011,
-  "Laysan Crake" => 1923
+  'Tasmanian Tiger' => 1936,
+  'Eastern Hare Wallaby' => 1890,
+  'Dodo' => 1662,
+  'Pyrenean Ibex' => 2000,
+  'Passenger Pigeon' => 1914,
+  'West African Black Rhinoceros' => 2011,
+  'Laysan Crake' => 1923
 }
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
