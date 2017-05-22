e8006bc715efe6f0e5f8c0f9ed798509cd644420
kunomaclis <mitchellhendee@gmail.com>
2017-05-22 16:56:42 -0500

    finish all questions

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -35,3 +35,35 @@ tucker.speak(3)
 tucker.roll_over
 tucker.dog_year(3)
 tucker.shake
+
+class Songs
+  def initialize
+    puts 'initializing song shenanigans'
+  end
+
+  def play
+    puts 'song has been favorited!'
+  end
+
+  def repeat(n)
+    amount = n
+    puts "song will repeat #{amount} times."
+  end
+end
+
+data = []
+loop_count = 0
+until loop_count == 50
+  song = Songs.new
+  song.play
+  song.repeat(3)
+  loop_count += 1
+  data << song
+end
+
+p data
+
+data.each do |instance|
+  instance.play
+  instance.repeat(2)
+end
