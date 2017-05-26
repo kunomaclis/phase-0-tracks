514ebc595ec78cb0aa3b435af1009ea47fee2d56
kunomaclis <mitchellhendee@gmail.com>
2017-05-25 19:53:27 -0500

    add santa array and begin adding attributes to class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,9 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = %w[Rudolph Dasher Dancer Prancer Vixen Comet Cupid Donner Blitzen]
+    @age = 0
     puts 'Initializing Santa instance...'
   end
 
@@ -12,7 +16,16 @@ class Santa
   end
 end
 
-santa = Santa.new
+# Initial driver code
 
-santa.speak
-santa.eat_milk_and_cookies("choclate chip")
+# santa = Santa.new
+# santa.speak
+# santa.eat_milk_and_cookies("choclate chip")
+
+santas = []
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
