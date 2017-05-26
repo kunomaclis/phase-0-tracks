8500d8c0be000f555ef93f815f6eddcfe8344016
kunomaclis <mitchellhendee@gmail.com>
2017-05-25 20:32:43 -0500

    play with gender and favorite reindeer

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,6 +14,24 @@ class Santa
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}!"
   end
+
+  # getter methods for attributes
+  attr_reader :age
+
+  attr_reader :ethnicity
+
+  # setter methods
+  def celebrate_birthday=(_integer)
+    @age + 1
+  end
+
+  def get_mad_at=(reindeer)
+    @reindeer_ranking.rotate(reindeer)
+  end
+
+  def gender_bender=(new_gender)
+    @gender = new_gender
+  end
 end
 
 # Initial driver code
@@ -22,10 +40,15 @@ end
 # santa.speak
 # santa.eat_milk_and_cookies("choclate chip")
 
-santas = []
+# santas = []
 
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# example_genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fluid', 'N/A']
+# example_ethnicities = ['black', 'Latino', 'white', 'Japanese-African', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i],)
+# end
+
+santa = Santa.new('female', 'white')
+santa.speak
+santa.gender_bender= "male"
+
