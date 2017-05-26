f34aedc4450b7fb4728e9177f6e80f7a25d92d3f
kunomaclis <mitchellhendee@gmail.com>
2017-05-26 18:32:52 -0500

    finish print of random santas with cookie randomization

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,5 @@
 class Santa
-
-  attr_reader :age, :ethnicity
+  attr_reader :age, :ethnicity, :reindeer_ranking
 
   attr_accessor :gender
 
@@ -17,30 +16,40 @@ class Santa
   end
 
   def eat_milk_and_cookies(cookie)
-    puts "That was a good #{cookie}!"
+    puts "That was a good #{cookie} cookie!"
   end
 
   # setter methods
-  def celebrate_birthday(num)
-    @age += 1 * num
+  def celebrate_birthday
+    @age += 1
   end
 
   def get_mad_at=(reindeer)
-    @reindeer_ranking.rotate(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking.insert(-1, reindeer_name)
   end
-
 end
 
 # Initial driver code
 
-# santa = Santa.new
-# santa.speak
-# santa.eat_milk_and_cookies("choclate chip")
+santa = Santa.new('male', 'white')
+santa.speak
+santa.eat_milk_and_cookies('choclate chip')
 
-# santas = []
+# Array test
 
-# example_genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fluid', 'N/A']
-# example_ethnicities = ['black', 'Latino', 'white', 'Japanese-African', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
-# example_genders.length.times do |i|
-#   santas << Santa.new(example_genders[i], example_ethnicities[i],)
-# end
+santas = []
+
+example_genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fluid', 'N/A']
+example_ethnicities = ['black', 'Latino', 'white', 'Japanese-African', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
+example_cookies = ['chocolate chip', 'mint', 'waffle', 'white macadamia', 'oatmeal', 'sugar']
+example_genders.length.upto(500) do |_i|
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+end
+
+# Take the santa hash and print out random santas!
+
+santas.each do |x|
+  puts "This Santa is an #{rand(0 - 140)} year old #{x.ethnicity} #{x.gender}. When given their favorite cookie they had this to say: #{x.eat_milk_and_cookies(example_cookies.sample)}"
+  puts '--------------------------'
+end
