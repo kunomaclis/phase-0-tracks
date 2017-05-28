e54d805f6320ba2089acfa20a1570d1636ae2edf
kunomaclis <mitchellhendee@gmail.com>
2017-05-27 21:53:01 -0500

    add class mixin with shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,40 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + '!!!' + ' :('
+#   end
+
+#   def self.yelling_happily(words)
+#     words + '!!!' + ' :D'
+#   end
+# end
+
+# puts Shout.yell_angrily("I'm mad")
+# puts Shout.yelling_happily("I'm happy")
+
+# ------------ MIXIN SHOUT MODULE
+
 module Shout
-  def self.yell_angrily(words)
-    words + '!!!' + ' :('
+  def yell_confusedely(words)
+    puts words + '???' ' :O'
   end
 
-  def self.yelling_happily(words)
-    words + '!!!' + ' :D'
+  def yell_awkwardly(words)
+    puts words + '...' 'xDDDDDD'
   end
 end
 
-puts Shout.yell_angrily("I'm mad")
-puts Shout.yelling_happily("I'm happy")
+class Protaganist
+  include Shout
+end
 
+class Sidekick
+  include Shout
+end
+
+protag = Protaganist.new
+protag.yell_confusedely('Where is my wife')
+protag.yell_awkwardly('are you my wife')
+
+robin = Sidekick.new
+robin.yell_confusedely('Where is the joker')
+robin.yell_awkwardly("I'm a real hero")
