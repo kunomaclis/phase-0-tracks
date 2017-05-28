3c814c0718056ade822fedf148407d6a3b241502
kunomaclis <mitchellhendee@gmail.com>
2017-05-28 00:22:35 -0500

    test with driver code. no success

--- ruby/game/hang_ruby.rb
+++ ruby/game/hang_ruby.rb
@@ -1,19 +1,33 @@
 class Game
-    def initialize(secret_word)
-        @secret_word = secret_word
-        @guess_count = secret_word.length
-        @is_over = false
-        @blanks_filled = "_" * @secret_word.length
-        @blanks_filled = @blanks_filled.split("")
-    end
-    
-    def split
-        @cups = @cups.split("")
-    end
+  attr_reader :blanks_filled, :guess_count, :is_over
 
-    def compare(guessed_word)
-        @guessed_word = guessed_word.split("")
-        @guess_count -= 1
-    @guessed_word.each do |letter| 
-        if @guessed_word[letter] == @secret_word[letter]
-        
+  def initialize(secret_word)
+    @secret_word = secret_word
+    @guess_count = secret_word.length
+    @is_over = false
+    @blanks_filled = '_' * @secret_word.length
+    @blanks_filled = @blanks_filled.split('')
+  end
+
+  def split
+    @secret_word = @secret_word.split('')
+  end
+
+  def compare(guessed_word)
+    @guessed_word = guessed_word.split('')
+    @guess_count -= 1
+    @guessed_word.each do |letter|
+      if @guessed_word[letter] == @secret_word[letter]
+        @blanks_filled.map { |_x| x = @guessed_word[letter] }
+      else
+        puts 'idk'
+      end
+    end
+  end
+end
+
+puts "Let's try this out"
+test = Game.new('Porcelein')
+
+puts 'Now to cover up the word...'
+test.split
