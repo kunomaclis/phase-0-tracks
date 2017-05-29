17e5b68a356b9da14ec1ee0a1e44bcc2942e2465
kunomaclis <mitchellhendee@gmail.com>
2017-05-28 21:19:36 -0500

    finish conditional loop for duplicate word

--- ruby/game/hang_ruby.rb
+++ ruby/game/hang_ruby.rb
@@ -1,50 +1,54 @@
 class Game
-  attr_accessor :guess_count, :is_over, :dashes, :history, :secret_word
+  attr_accessor :history, :dashes, :secret_word, :guess_count
 
-  def initialize(word)
-    @secret_word = word.to_str
-    @secret_word = @secret_word.split('')
-    @guess_count = @secret_word.length - 1
+  def initialize(input)
+    @secret_word = input.split('')
     @dashes = '_' * @secret_word.length
     @dashes = @dashes.split('')
+    @history = []
+    @guess_count = @secret_word.length
   end
 
   def compare(user_guess)
     @guessed_word = user_guess.split('')
     @secret_word.each do |letter|
-      next unless @guessed_word.include?(letter)
-      @index_matcher = @secret_word.index(letter)
-      @dashes[@index_matcher] = letter
-      @guess_count -= 1
+      if @guessed_word.include?(letter)
+        @index_matcher = @secret_word.index(letter)
+        @dashes[@index_matcher] = letter
+      end
     end
-    if @dashes == @secret_word
+    if @guessed_word == @secret_word
       @guess_count = 0
     else
-      p @dashes
-      p "You have #{@guess_count} guesses left!"
+      @guess_count -= 1
     end
+    puts "You have #{guess_count} guesses left!"
+    p @dashes
 
     def duplicate_check(user_guess)
-      @history = []
       @history << user_guess
       @history
     end
   end
-end
 
-puts "Let's try this out, give me a secret word:"
-user_input = gets.chomp
-test = Game.new(user_input)
-while @guess_count != 0
-  puts 'Guess a word for me:'
-  user_guess = gets.chomp.to_str
-  test.compare(user_guess)
-  test.duplicate_check(user_guess)
+  puts "Let's try this out, give me a secret word:"
+  initial_input = gets.chomp
+  test = Game.new(initial_input)
 
-  break if @guess_count == 0
+  until test.guess_count.zero?
+    puts 'Guess a word for me:'
+    user_guess = gets.chomp.to_str
+    if test.history.include?(user_guess)
+      puts 'Hey, you typed this word already...'
+      test.guess_count += 1
+    end
+    test.compare(user_guess)
+    test.duplicate_check(user_guess)
+    break if test.guess_count.zero?
   end
-if @secret_word == @dashes
-  puts 'Congratulations! you guessed the word!'
-elsif @secret_word != @dashes
-  puts "Welp, you didn't get it. the word was #{@secret_word.join}"
+  if test.secret_word == test.dashes
+    puts 'Congratulations! you guessed the word!'
+  elsif test.secret_word != test.dashes
+    puts "Welp, you didn't get it. the word was #{test.secret_word.join}"
+  end
 end
