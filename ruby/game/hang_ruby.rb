5345f65c6b207fbe21f3249f60b398cb82944c44
kunomaclis <mitchellhendee@gmail.com>
2017-05-28 21:40:33 -0500

    finish program and create hobo workaround for edge case

--- ruby/game/hang_ruby.rb
+++ ruby/game/hang_ruby.rb
@@ -1,16 +1,20 @@
+# To make word input hidden
+require 'io/console'
+
 class Game
-  attr_accessor :history, :dashes, :secret_word, :guess_count
+  attr_accessor :history, :dashes, :secret_word, :guess_count, :guessed_word
 
   def initialize(input)
-    @secret_word = input.split('')
+    @secret_word = input.downcase.chars
     @dashes = '_' * @secret_word.length
-    @dashes = @dashes.split('')
+    @dashes = @dashes.chars
     @history = []
     @guess_count = @secret_word.length
   end
 
   def compare(user_guess)
-    @guessed_word = user_guess.split('')
+    @guessed_word = user_guess.downcase.chars
+    # compare letters of guessed word to secret word. and fill in dashes index
     @secret_word.each do |letter|
       if @guessed_word.include?(letter)
         @index_matcher = @secret_word.index(letter)
@@ -22,8 +26,6 @@ class Game
     else
       @guess_count -= 1
     end
-    puts "You have #{guess_count} guesses left!"
-    p @dashes
 
     def duplicate_check(user_guess)
       @history << user_guess
@@ -31,24 +33,28 @@ class Game
     end
   end
 
-  puts "Let's try this out, give me a secret word:"
-  initial_input = gets.chomp
-  test = Game.new(initial_input)
-
-  until test.guess_count.zero?
+  puts 'Welcome to Hang_ruby. Please type a secret word for a victim to guess'
+  # hidden input for funsies
+  initial_input = STDIN.noecho(&:gets).chomp
+  game = Game.new(initial_input)
+  # loop for guess count
+  until game.guess_count.zero?
     puts 'Guess a word for me:'
     user_guess = gets.chomp.to_str
-    if test.history.include?(user_guess)
+    if game.history.include?(user_guess)
       puts 'Hey, you typed this word already...'
-      test.guess_count += 1
+      game.guess_count += 1
     end
-    test.compare(user_guess)
-    test.duplicate_check(user_guess)
-    break if test.guess_count.zero?
+    game.compare(user_guess)
+    game.duplicate_check(user_guess)
+    break if game.guess_count.zero?
+    puts "You have #{game.guess_count} guesses left!"
+    p game.dashes
   end
-  if test.secret_word == test.dashes
+  # Win/lose condition. Win condition is unique as there is an edge case with my index/dash. If using @dashes to refer a win will not come up if multiple letters are in a word. my 'hack' is to just use @guessed_word to shoot the count to zero and rely on it to compare it to secret. but still... a definite hack.
+  if game.secret_word == game.guessed_word
     puts 'Congratulations! you guessed the word!'
-  elsif test.secret_word != test.dashes
-    puts "Welp, you didn't get it. the word was #{test.secret_word.join}"
+  elsif game.secret_word != game.dashes
+    puts "Welp, you didn't get it. the word was #{game.secret_word.join}"
   end
 end
