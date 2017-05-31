6a25d14ded5b9fb73da291ffbfe7ca73e7cec50f
kunomaclis <mitchellhendee@gmail.com>
2017-05-31 11:20:21 -0500

    add string reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,17 @@
 // String reverse function
 // Takes in a string and prints out a reversed version "hello" => "olleh"
+// split the string into an array, then reverse the array in place. 
+// join the strings together
+
+function reverseString(str) {
+    // split the method to an array
+    var splittedString = str.split("");
+    // reverse the array
+    var reverseArray = splittedString.reverse();
+    // join the reversed array into a string
+    var joinArray = reverseArray.join("");
+    // return that string
+    return joinArray;
+}
+
+console.log(reverseString("swagbag"));
