43568e108dd417f94113ede974e238972e4ae31f
kunomaclis <mitchellhendee@gmail.com>
2017-05-31 20:25:56 -0500

    refactor methods and explain purpose of private method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -16,39 +16,47 @@ class VirusPredictor
     @population = population
     @population_density = population_density
   end
+
   # Public class method that when called upon uses instance variables from initialization to run private methods.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
+  # The purpose of private is to prevent any methods below the syntax from being accessed outside of the class
+
   private
-  # Takes instance variables as parameters 
+
+  attr_reader :population_density, :population, :state
+  # def population_density
+  #   @population_density
+  # end
+
+  # Takes instance variables as parameters
   # Uses if condition from highest to lowest population density
   # if population density meets a condition. new variable number of deaths
   # is equal to population multipled times a fraction
   # number_of_deaths decimal is rounded down to nearest integer
   # print a message stating how many deaths will happen
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
+    number_of_deaths = if population_density >= 200
+                         (population * 0.4).floor
+                       elsif population_density >= 150
+                         (population * 0.3).floor
+                       elsif population_density >= 100
+                         (population * 0.2).floor
+                       elsif population_density >= 50
+                         (population * 0.1).floor
+                       else
+                         (population * 0.05).floor
+                       end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
+    print "#{state} will lose #{number_of_deaths} people in this outbreak"
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread # in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
 
@@ -58,29 +66,26 @@ class VirusPredictor
     # print a message stating how quickly the virus spreads
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
+    speed += if population_density >= 200
+               0.5
+             elsif population_density >= 150
+               1
+             elsif population_density >= 100
+               1.5
+             elsif population_density >= 50
+               2
+             else
+               2.5
+             end
 
     puts " and will spread across the state in #{speed} months.\n\n"
-
   end
-
 end
 
 #=======================================================================
 
 # DRIVER CODE
- # initialize VirusPredictor for each state
-
+# initialize VirusPredictor for each state
 
 # alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 # alabama.virus_effects
@@ -97,18 +102,16 @@ end
 # Iteration driver code for release 4
 
 STATE_DATA.each do |state, data|
-# p state
-# p data[:population_density]
-# p data[:population]
+  # p state
+  # p data[:population_density]
+  # p data[:population]
 
-# for each state in state data nested data structure
-# create new instance of virus predictor using state key's population density and population values
-# also run the method virus_effects into this instance
+  # for each state in state data nested data structure
+  # create new instance of virus predictor using state key's population density and population values
+  # also run the method virus_effects into this instance
   current_state = VirusPredictor.new(state, data[:population_density], data[:population])
   current_state.virus_effects
-
 end
-
 
 #=======================================================================
 # Reflection Section
