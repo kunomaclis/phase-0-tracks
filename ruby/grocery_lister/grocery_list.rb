1f61e8d36bc512cddb7a6b732e674d37bf9c8fb1
kunomaclis <mitchellhendee@gmail.com>
2017-06-10 16:07:00 -0500

    successfully implement add method to grocery list program

--- ruby/grocery_lister/grocery_list.rb
+++ ruby/grocery_lister/grocery_list.rb
@@ -33,4 +33,37 @@ list.each do |item|
   puts "#{item['quantity']} #{item['name']}"
 end
 
+# Methods for user interaction
 
+def add_item(db, item, quantity)
+  db.execute('INSERT INTO list (name, quantity) VALUES (?, ?)', [item], [quantity])
+end
+
+def remove_item(db, item)
+  db.execute('DELETE FROM list WHERE name=(?)', [item])
+end
+
+# Begin program:
+
+puts '-' * 62
+puts "Hendee Grocery List Checker: Because it's best to be prepared!"
+puts '-' * 62
+
+puts 'You can add, remove, edit, or review:'
+guidance_choice = gets.chomp
+if guidance_choice == 'add'
+  continue = ''
+  until continue == 'no'
+    puts 'What item would you like to add?'
+    item_to_add = gets.chomp
+    break if item_to_add == 'no'
+    puts 'How many of that will you need?'
+    item_quantity = gets.chomp
+    break if item_to_add == 'no'
+    puts "Okay, adding #{item_quantity} #{item_to_add} to the list!"
+    add_item(db, item_to_add, item_quantity)
+    puts 'Would you like to add more items?'
+    continue = gets.chomp.downcase
+    break if continue == 'no'
+  end
+end
